f01d0a2f6bd57c936b3c5c46963a7f5bb8194781
Stefan Lau <stefan.lau@holidaycheck.com>
2017-11-24 18:03:08 +0100

    Introduce theme

---
+++ theme/base16Tomorrow.nix
@@ -0,0 +1,13 @@
+{
+  background = "#1d1f21";
+  foreground = "#c5c8c6";
+  black = "#1d1f21";
+  gray = "#969896";
+  red = "#cc6666";
+  green = "#b5bd68";
+  yellow = "#f0c674";
+  blue = "#81a2be";
+  magenta = "#b294bb";
+  cyan = "#8abeb7";
+  white = "#ffffff";
+}
